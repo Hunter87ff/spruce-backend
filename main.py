9d350c8d27351a7262f9dc709f61ac5dd697bad9
hunter87 <83773586+Hunter87ff@users.noreply.github.com>
2024-11-30 00:38:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from app import app
+import os
 from routes.auth import auth
 from routes._guild import guild
 
@@ -6,4 +7,5 @@ from routes._guild import guild
 app.register_blueprint(auth)
 app.register_blueprint(guild) # subdomain="guild"
 
-app.run(port=3001, host="0.0.0.0", debug=True)
+of os.getenv("dev"):
+  app.run(port=3001, host="0.0.0.0", debug=True)
